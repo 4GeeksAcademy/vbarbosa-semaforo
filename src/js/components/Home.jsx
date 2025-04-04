9ae9ca13d06c350eb9cf9f859283ba815c2df1eb
Vbarbosa <131600563+VitoriaBSouza@users.noreply.github.com>
2025-04-04 20:08:41 +0000

    made semaforo, css and complement

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,26 @@
 import React from "react";
+import { Semaforo } from "./Semaforo";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col-12 col-lg-12">
+					<div className="position-absolute top-50 start-50 translate-middle">
+						<div className="bg-dark topSemaforo mx-auto"></div>
+						<div className="bg-dark semaforo mx-auto">
+							<div className="luzRoja m-2 mx-auto">
+								<Semaforo />
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
